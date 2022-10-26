3aa6513c11a1a87f29f413cb8c02195d5e6fa495
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2022-10-26 11:05:48 +0200

    Accueil entièrement responsive

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,23 @@ import footerVue from '@/components/footer.vue';
         <headerVue class="mt-10"></headerVue>
 
 
-        <div class="flex flex-col lg:flex-row-reverse items-center justify-center">
+        <div class="flex flex-col lg:flex-row-reverse items-center justify-center 1440:mx-40 ">
 
 
-          <div class="w-3/4 items-center lg:mr-12">
-            <img src="images/mk_accueil.png" />
+          <div class=" items-center  lg:mr-12  ">
+            <img class="w-520 px-4 lg:px-0" src="images/mk_accueil.png" />
           </div>
-          <div class="lg:flex lg:flex-col lg:items-start">
+          <div class="lg:flex lg:flex-col lg:items-start  1440:text-xl 1440:w-4/5">
             <div class="my-4 lg:ml-8">
-              <p class="text-white font-inter text-center text-4xl">SmartWatch</p>
+              <p class="text-white font-inter text-center text-2xl lg:text-4xl">SmartWatch</p>
             </div>
 
             <div class="lg:ml-8">
-              <p class="text-white font-inter text-center text-3xl">Serie P</p>
+              <p class="text-white font-inter text-center text-xl lg:text-3xl">Serie P</p>
             </div>
 
             <div class="mx-8 mt-4 lg:w-9/12">
-              <p class="text-white shrink-0 inline-block font-inter ">
+              <p class="text-white shrink-0 inline-block text-sm lg:text-base font-inter ">
                 Découvrez la nouvelle gamme de montre
                 connecté
                 SmartWatch Serie P de TiKTaK..
@@ -42,122 +42,103 @@ import footerVue from '@/components/footer.vue';
               </button>
             </div>
           </div>
-
-
         </div>
 
 
-        <!-- <div class="flex">
-          <div class=" mt-40 w-2/5 lg:w-1/2 ">
-            <div class="">
-              <p class="text-white font-inter text-4xl ml-52">SmartWatch</p>
-            </div>
+        <div class="flex justify-center">
+          <div class="my-20 w-4/5 border-b-default border-white"></div>
+        </div>
 
-            <div class="mt-4  ">
-              <p class="text-white font-inter text-3xl ml-52">Serie P</p>
-            </div>
 
-            <div class="mt-4 pt-9">
-              <p class="text-white shrink-0 inline-block font-inter text-2xl lg:text-base ml-52">
-                Découvrez la nouvelle gamme de montre
-                connecté
-                SmartWatch Serie P de TiKTaK..
-                Nouveau design, nouvelles fonctionnalités mais surtout
-                entiérement personnalisable selon vos envies !</p>
-            </div>
-
-            <div class="flex">
-              <div class="mt-4 pt-20">
-                <button
-                  class=" lg:text-lg text-black font-inter text-2xl ml-52 bg-white py-2 px-10 rounded-full hover:bg-jaune-0">
-                  Personnaliser
-                </button>
+        <div class="lg:flex lg:justify-evenly">
+          <div class="">
+            <div class="flex justify-center">
+              <div class="w-40 md:w-60">
+                <img class="" src="images/montre_noir.png" />
               </div>
             </div>
+
+            <div class="text-center ">
+              <p class="text-jaune-0 font-medium text-xl">Nouveauté</p>
+              <p class="font-bold text-xl text-white mt-2 lg:text-xl">Boîtier en aluminium noir,</p>
+              <p class="font-bold text-xl text-white lg:text-xl">stellaire, boucle sport</p>
+              <p class="font-thin text-sm text-white my-3">À partir de 299€</p>
+              <button class="text-black font-inter text-xl bg-jaune-0 py-1 px-8 rounded-full">
+                Personnaliser
+              </button>
+            </div>
           </div>
 
-          <div class="mt-36 mx-36">
-            <img src="images/mk_accueil.png" />
+          <div class="mt-12 lg:m-0">
+            <div class="flex justify-center">
+              <div class="w-40 md:w-60">
+                <img src="images/montre_rouge.png" />
+              </div>
+            </div>
+
+            <div class="text-center ">
+              <p class="text-jaune-0 font-medium text-xl">Nouveauté</p>
+              <p class="font-bold text-xl text-white mt-2 lg:text-xl">Boîtier en aluminium noir,</p>
+              <p class="font-bold text-xl text-white lg:text-xl">stellaire, boucle sport</p>
+              <p class="font-thin text-sm text-white my-3">À partir de 299€</p>
+              <button class="text-black font-inter text-xl bg-jaune-0 py-1 px-8 rounded-full">
+                Personnaliser
+              </button>
+            </div>
+          </div>
+
+          <div class="mt-12 lg:m-0">
+            <div class=" flex justify-center">
+              <div class="w-40 md:w-60">
+                <img src="images/montre_jaune.png" />
+              </div>
+            </div>
+
+            <div class="text-center ">
+              <p class="text-jaune-0 font-medium text-xl">Nouveauté</p>
+              <p class="font-bold text-xl text-white mt-2 lg:text-xl">Boîtier en aluminium noir,</p>
+              <p class="font-bold text-xl text-white lg:text-xl">stellaire, boucle sport</p>
+              <p class="font-thin text-sm text-white my-3">À partir de 299€</p>
+              <button class="text-black font-inter text-xl bg-jaune-0 py-1 px-8 rounded-full">
+                Personnaliser
+              </button>
+            </div>
           </div>
         </div>
 
 
         <div class="flex justify-center">
-          <div class="mt-40 w-4/5 border-b-default border-white"></div>
+          <div class="my-20 w-4/5 border-b-default border-white"></div>
         </div>
 
 
-        <div class="flex justify-evenly">
-          <div class="mt-28 lg:w-72">
-            <img src="images/montre_noir.png" />
-            <div class="text-center mt-12 ">
-              <p class="text-jaune-0 font-medium text-xl">Nouveauté</p>
-              <p class="font-bold text-2xl text-white mt-2 lg:text-xl">Boîtier en aluminium noir,</p>
-              <p class="font-bold text-2xl text-white lg:text-xl">stellaire, boucle sport</p>
-              <p class="font-thin text-xl text-white my-3">À partir de 299€</p>
-              <button class="text-black font-inter text-2xl bg-jaune-0 py-2 px-10 rounded-full">
-                Personnaliser
-              </button>
-            </div>
-          </div>
-
-          <div class="mt-28 lg:w-72">
-            <img src="images/montre_rouge.png" />
-            <div class="text-center mt-12">
-              <p class="text-jaune-0 font-medium text-xl">Nouveauté</p>
-              <p class="font-bold text-2xl text-white lg:text-xl mt-2">Boîtier en aluminium noir,</p>
-              <p class="font-bold text-2xl text-white lg:text-xl">stellaire, boucle sport</p>
-              <p class="font-thin text-xl text-white my-3">À partir de 299€</p>
-              <button class="text-black font-inter text-2xl bg-jaune-0 py-2 px-10 rounded-full">
-                Personnaliser
-              </button>
-            </div>
-          </div>
-
-          <div class="mt-28 lg:w-72">
-            <img src="images/montre_jaune.png" />
-            <div class="text-center mt-12">
-              <p class="text-jaune-0 font-medium text-xl">Nouveauté</p>
-              <p class="font-bold text-2xl text-white mt-2 lg:text-xl">Boîtier en aluminium noir,</p>
-              <p class="font-bold text-2xl text-white lg:text-xl">stellaire, boucle sport</p>
-              <p class="font-thin text-xl text-white my-3">À partir de 299€</p>
-              <button class="text-black font-inter text-2xl bg-jaune-0 py-2 px-10 rounded-full">
-                Personnaliser
-              </button>
-            </div>
-          </div>
-        </div>
-
-
-
-        <div class="flex justify-center">
-          <div class="mt-40 w-4/5 border-b-default border-white"></div>
-        </div>
-
-        <div class="mt-28">
-          <div class="flex justify-evenly ">
-            <div class="w-490">
+        <div class="lg:flex lg:justify-center ">
+          <div class="flex justify-center">
+            <div class="w-64 mb-12 md:w-490">
               <img class="" src="images/double_mk.png" />
             </div>
+          </div>
 
-            <div>
-              <p class="text-white lg:mt-0 mt-28 font-bold font-inter text-3xl">Pas à votre goût</p>
-              <div class="text-2xl font-bold my-14 text-white">
-                <p>Personnaliser à volonté la SmartWatch</p>
-                <p>Serie P pour qu'elle corresponde </p>
-                <p>à vos goûts grâce à l'interface </p>
-                <p>de personnalisation !</p>
-              </div>
-              <button class="text-black font-inter text-2xl bg-jaune-0 py-2 px-10 rounded-full">
+          <div class="lg:mt-20">
+            <p class="text-white lg:mt-0 lg:text-4xl font-bold font-inter text-base text-center">Pas à votre goût ?</p>
+            <div class="text-xl mx-10 lg:text-xl lg:mt-8 mt-4 text-white text-center">
+              <p class="lg:text-left">Personnaliser à volonté votre SmartWatch</p>
+              <p class="hidden lg:block text-left">Serie P pour qu'elle corresponde à votre</p>
+              <p class="hidden lg:block text-left ">Style grâce à l'interface de personnalisation</p>
+            </div>
+            <div class="flex justify-center">
+              <button
+                class=" mt-4 text-black lg:text-2xl lg:mt-12 font-inter text-base bg-jaune-0 py-1 px-8 rounded-full ">
                 Interface de personnalisation
               </button>
             </div>
           </div>
         </div>
 
+
         <div class="flex justify-center">
-          <div class="mt-40 w-4/5 border-b-default border-white"></div>
-        </div> -->
+          <div class="mt-20 w-4/5 border-b-default border-white"></div>
+        </div>
 
         <footerVue></footerVue>
 
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const menuVisible = ref(true);
+</script>
+
 <template>
     <div>
         <div class="">
@@ -8,23 +13,104 @@
 
                 <div class=" hidden text-white lg:flex items-center space-x-6">
                     <router-link class=" no-underline" to="/">
-                        <code class="font-inter hover:border-b-default border-b-white ">Nouveauté</code>
+                        <p class="font-inter hover:border-b-default border-b-white ">Nouveauté</p>
                     </router-link>
                     <router-link class="text-white no-underline" to="/connexion">
-                        <code class="font-inter hover:border-b-default border-b-white">Personnalisation</code>
+                        <p class="font-inter hover:border-b-default border-b-white">Personnalisation</p>
                     </router-link>
                     <router-link class="text-white shrink-0 no-underline" to="/liste">
-                        <code class="font-inter hover:border-b-default  border-b-white">Vos montres</code>
+                        <p class="font-inter hover:border-b-default  border-b-white">Vos montres</p>
                     </router-link>
                     <router-link class="text-white  no-underline" to="/liste-fetch">
-                        <code class="font-inter hover:border-b-default border-b-white">Promotions</code>
+                        <p class="font-inter hover:border-b-default border-b-white">Promotions</p>
                     </router-link>
                     <router-link class="text-white no-underline" to="/edit/new">
-                        <code><img class="hover:border-b-default border-b-white" src="public/images/user.svg"/></code>
+                        <img class="hover:border-b-default border-b-white" src="public/images/user.svg" />
                     </router-link>
 
                 </div>
             </div>
         </div>
+
+        <div class="lg:hidden flex items-center">
+            <button v-on:click="menuVisible = !menuVisible" class="">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black" viewBox="0 0 20 20"
+                    fill="currentColor" :class="{ hidden: !menuVisible }">
+                    <path fill-rule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                        clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
+
+        <div class="" :class="{ hidden: menuVisible }">
+            <button v-if="!menuVisible" v-on:click="menuVisible = !menuVisible" class="">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+            <h4 class="text-xl">
+                <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
+                Naviguer à travers le menu
+            </h4>
+            <ul class="ml-8">
+                <li>
+                    <router-link class="text-black no-underline hover:text-indigo-400" to="/">
+                        <code class="font-mono">Accueil</code>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link class="text-black no-underline hover:text-indigo-400" to="/simple">
+
+                        <code class="font-mono">Simple.vue</code>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link class="text-black  no-underline hover:text-indigo-700" to="/liste">
+                        <code class="font-mono">Liste.vue</code>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link class="text-black  no-underline hover:text-indigo-400" to="/liste-fetch">
+                        <code class="font-mono">Liste-Fetch</code>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link class="text-black  no-underline hover:text-indigo-400" to="/offre/1">
+                        <code class="font-mono">Offre nbr1</code>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link class="text-black  no-underline hover:text-indigo-400" to="/offre/2">
+                        <code class="font-mono">Offre nbr2</code>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link class="text-black  no-underline hover:text-indigo-400" to="/edit/new">
+                        <code class="font-mono">New.vue</code>
+                    </router-link>
+                </li>
+
+                <li>
+                    <router-link class="text-black  no-underline hover:text-indigo-400" to="/liste-supabase">
+                        <code class="font-mono">Liste-Supabase</code>
+                    </router-link>
+                </li>
+
+                <li>
+                    <router-link class="text-black  no-underline hover:text-indigo-400" to="/authentification">
+                        <code class="font-mono">Authentification</code>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link class="text-black  no-underline hover:text-indigo-400" to="/edit/id ">
+                        <code class="font-mono">Edit</code>
+                    </router-link>
+                </li>
+
+            </ul>
+        </div>
+
     </div>
 </template>
