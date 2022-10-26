7ae69f745bb7f9478801246f3340ba6ff20a815c
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2022-10-26 14:23:25 +0200

    Peronnalisation responsive

--- src/pages/PersonnalisationView.vue
+++ src/pages/PersonnalisationView.vue
@@ -20,11 +20,57 @@ import montreSvgVue from '@/components/montreSvg.vue';
             </div>
 
             <div class="">
-                <p class="text-center text-white text-xl">Bienvenue sur l'interface de personnalisation</p>
-                <p class="text-center text-white text-md mt-4">Choississez une partie et une couleur à modifier</p>
+                <p class="text-center font-inter text-white text-xl">Bienvenue sur l'interface de personnalisation</p>
+                <p class="text-center font-inter text-white text-md mt-4">Choississez une partie et une couleur à
+                    modifier</p>
+            </div>
+            <!-- 
+            <FormKit>
+               Ajouter les inputs 
+            </FormKit>
+            -->
+
+            <div class="flex justify-center">
+                <button
+                    class=" mt-4 text-black lg:text-2xl lg:mt-12 font-inter text-base bg-jaune-0 py-1 px-8 rounded-full ">
+                    Enregistrer
+                </button>
             </div>
 
 
+            <div class="flex justify-center">
+                <div class="mt-20 w-4/5 border-b-default border-white"></div>
+            </div>
+
+            <div class="text-center text-white text-xl my-4">
+                <p>Vos montres</p>
+            </div>
+
+            <div class="lg:flex lg:justify-evenly mb-6">
+                <div class="">
+                    <div class="flex justify-center">
+                        <div class="w-40 md:w-60">
+                            <img class="" src="images/montre_noir.png" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="lg:flex lg:justify-evenly">
+                <div class="">
+                    <div class="flex justify-center">
+                        <div class="w-40 md:w-60">
+                            <img class="" src="images/montre_jaune.png" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex justify-center">
+                <div class="mt-10 w-4/5 border-b-default border-white"></div>
+            </div>
+
+            <footerVue></footerVue>
         </div>
     </div>
 </template>
